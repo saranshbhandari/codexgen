f9b56717fa80b5cf9a0d079a67a768816e0c92bc
saranshbhandari <37260811+saranshbhandari@users.noreply.github.com>
2026-03-09 13:57:20 +0530

    Add LiteralExpression class for evaluating literals

--- new/LiteralExpression.java
+++ new/LiteralExpression.java
@@ -1,1 +1,15 @@
+package variable;
 
+final class LiteralExpression implements Expression {
+
+    private final Object value;
+
+    LiteralExpression(Object value) {
+        this.value = value;
+    }
+
+    @Override
+    public Object eval(VariableStore store) {
+        return value;
+    }
+}
